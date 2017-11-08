7a01f9629cba392a2dd9555897914cf1d92fc486
Brian Byers <brian@xclo.io>
2017-11-08 14:01:51 -0700

    adding jdbc

--- databases/tracker/create_databases.sql
+++ databases/tracker/create_databases.sql
@@ -8,3 +8,13 @@ CREATE USER IF NOT EXISTS 'tracker'@'localhost'
   IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON tracker_dev.* TO 'tracker' @'localhost';
 GRANT ALL PRIVILEGES ON tracker_test.* TO 'tracker' @'localhost';
+
+CREATE USER IF NOT EXISTS 'tracker'@'127.0.0.1'
+  IDENTIFIED BY '';
+GRANT ALL PRIVILEGES ON tracker_dev.* TO 'tracker' @'127.0.0.1';
+GRANT ALL PRIVILEGES ON tracker_test.* TO 'tracker' @'127.0.0.1';
+
+CREATE USER IF NOT EXISTS 'tracker'@'172.17.0.1'
+  IDENTIFIED BY '';
+GRANT ALL PRIVILEGES ON tracker_dev.* TO 'tracker' @'172.17.0.1';
+GRANT ALL PRIVILEGES ON tracker_test.* TO 'tracker' @'172.17.0.1';
